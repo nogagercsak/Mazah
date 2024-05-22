2eecb962959c4f0f022a929e877e2139cdf9435c
Noga Gercsak <nogah0317@gmail.com>
2024-05-21 20:11:48 -0400

    Initial Commit

---
+++ Mazah/MazahApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazahApp.swift
+//  Mazah
+//
+//  Created by Noga Gercsak on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MazahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

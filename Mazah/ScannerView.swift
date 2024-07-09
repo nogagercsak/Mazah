d91fd2d8bfa40e670d6307acace357c309857f6e
gabriellengrvc <gabriellenguyengervacio@gmail.com>
2024-07-09 20:59:32 +0200

    Scanner View

--- Mazah/ScannerView.swift
+++ Mazah/ScannerView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 struct ScannerView: View {
     
     @Binding var showSignInView: Bool
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
         NavigationView {
@@ -21,17 +22,37 @@ struct ScannerView: View {
                     HStack {
                         Spacer()
                         NavigationLink(destination: AddFoodManualView()) {
-                            Text("Add Food Manually")
+                            Text("Add Food Manually +")
                                 .padding()
-                                .background(Color.blue)
+                                .frame(width: 214, height: 60)
+                                .background(Color(red: 0.62, green: 0.76, blue: 0.62))
                                 .foregroundColor(.white)
-                                .cornerRadius(8)
+                                .cornerRadius(10)
                                 .padding()
+                                .font(Font.custom("Poppins-Regular", size: 18))
                         }
                     }
                 }
             }
-            .navigationBarHidden(true)
+            .navigationBarItems(leading: Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            HStack {
+                                Image(systemName: "chevron.left")
+                                    .foregroundColor(Color(red: 0.45, green: 0.68, blue: 0));
+                                Text("Go back to Foods")
+                                    .underline()
+                                    .foregroundColor(Color(red: 0.45, green: 0.68, blue: 0));
+                            }
+                        })
         }
+    }
+}
+
+struct ScannerView_Previews: PreviewProvider {
+    @State static var showSignInView = false
+    
+    static var previews: some View {
+        ScannerView(showSignInView: $showSignInView)
     }
 }

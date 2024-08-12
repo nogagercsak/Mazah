9e3ca546660aba0bf634a62df1be7da1ca54c6f8
gabriellengrvc <gabriellenguyengervacio@gmail.com>
2024-08-12 02:09:21 +0200

    Settings

--- Mazah/SettingsView.swift
+++ Mazah/SettingsView.swift
@@ -17,48 +17,72 @@ struct SettingsView: View {
     @State private var isEmailUpdated: Bool = false
     @State private var isPasswordUpdated: Bool = false
     @State private var errorMessage: String?
-
+    
     var body: some View {
-        VStack {
-            List {
-                Button("Log out") {
-                    Task {
-                        do {
-                            try viewModel.signOut()
-                            showSignInView = true
-                        } catch {
-                            print(error)
+        ZStack{
+            Color(hex: "#FFF6E2")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                List {
+                    Button("Log out") {
+                        Task {
+                            do {
+                                try viewModel.signOut()
+                                showSignInView = true
+                            } catch {
+                                print(error)
+                            }
                         }
                     }
-                }
-                
-                Button(role: .destructive) {
-                    Task {
-                        do {
-                            try await viewModel.deleteAccount()
-                            showSignInView = true
-                        } catch {
-                            print(error)
+                    .listRowBackground(Color.clear)
+                    .padding(8)
+                    .background(Color(red: 0.43, green: 0.51, blue: 0.42))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    
+                    Button(role: .destructive) {
+                        Task {
+                            do {
+                                try await viewModel.deleteAccount()
+                                showSignInView = true
+                            } catch {
+                                print(error)
+                            }
                         }
+                    } label: {
+                        Text("Delete account")
                     }
-                } label: {
-                    Text("Delete account")
+                    .listRowBackground(Color.clear)
+                    .padding(8)
+                    .background(.white)
+                    .foregroundColor(.red)
+                    .cornerRadius(10)
+                    emailSection
+                        .listRowBackground(Color.clear)
                 }
-                
-                emailSection
+                .listStyle(.plain)
+                .background(Color(hex: "#FFF6E2"))
+                .navigationTitle("Settings")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                    Text("Settings")
+                    .font(Font.custom("Poppins-Regular", size: 23))
+                    .foregroundColor(Color(red: 0.43, green: 0.51, blue: 0.42))
+                    .padding(.top, 0)
+                    }
+                }
+                                
+                                Spacer()
+                                
+                                NavBar(showSignInView: $showSignInView)
+                            }
+            .alert(isPresented: $isEmailUpdated) {
+                Alert(title: Text("Success"), message: Text("Email updated successfully"), dismissButton: .default(Text("OK")))
             }
-            .navigationTitle("Settings")
-            .navigationBarBackButtonHidden(true)
-            
-            Spacer()
-            
-            NavBar(showSignInView: $showSignInView)
-        }
-        .alert(isPresented: $isEmailUpdated) {
-            Alert(title: Text("Success"), message: Text("Email updated successfully"), dismissButton: .default(Text("OK")))
-        }
-        .alert(isPresented: $isPasswordUpdated) {
-            Alert(title: Text("Success"), message: Text("Password updated successfully"), dismissButton: .default(Text("OK")))
+            .alert(isPresented: $isPasswordUpdated) {
+                Alert(title: Text("Success"), message: Text("Password updated successfully"), dismissButton: .default(Text("OK")))
+            }
         }
     }
 }
@@ -111,6 +135,9 @@ extension SettingsView {
             }
         } header: {
             Text("Email & Password Functions")
+                .font(Font.custom("Poppins-Regular", size: 15))
+                .foregroundColor(Color(red: 0.43, green: 0.51, blue: 0.42))
+                .padding(.top, 0)
         }
     }
 }
